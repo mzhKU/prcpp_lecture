2e9c69322578417f78a3fe2e74c4fb62325a0137
mzhKU <ma.hed@bluewin.ch>
2020-09-28 00:31:56 +0200

    tests pass

--- testat01/PRCPP/MatrixMultiplication/dllmain.cpp
+++ testat01/PRCPP/MatrixMultiplication/dllmain.cpp
@@ -5,7 +5,6 @@
 
 // Output of .dll files: D:\00_fhnw\repo_prcpp\vs_projekte\testat01\PRCPP\x64
 
-using namespace std;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call)
@@ -19,13 +18,20 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     return TRUE;
 }
 
-void multiply(double* pa, double* pb, double* pr,
+void multiply(jdouble* pa, jdouble* pb, jdouble* pr,
     jint aRows, jint aCols, jint bCols) {
     for (size_t r = 0; r < aRows; r++) {
         for (size_t c = 0; c < bCols; c++) {
+
+            // Resetting the sum for every [i][j].
+            jdouble tmpSum = 0.0;
             for (size_t k = 0; k < aCols; k++) {
-                *(pr + r * bCols + c) += *(pa + r * aCols + k) * *(pb + k * bCols + c);
+                // *(pr + r * bCols + c) += *(pa + r * aCols + k) * *(pb + k * bCols + c);
+                // tmpSum += pa[r * aCols + k] * pb[k * bCols + c];
+                tmpSum += *(pa + r * aCols + k) * *(pb + k * bCols + c);
             }
+            // pr[r * bCols + c] = tmpSum;
+            *(pr + r * bCols + c) = tmpSum;
         }
     }
 }
@@ -35,49 +41,80 @@ JNIEXPORT void JNICALL Java_matrix_1multiplication_Matrix_multiplyC(JNIEnv* env,
     jdoubleArray aValues, jdoubleArray bValues, jdoubleArray result,
     jint           aRows, jint           aCols, jint          bCols) {
 
-    cout << "Entering C++ multiplication code." << endl;
+    std::cout << "Entering C++ multiplication code." << std::endl;
+
+    // https://docs.oracle.com/en/java/javase/15/docs/specs/jni/functions.html#array-operations
+    jboolean isCopyA;
+    jboolean isCopyB;
+    jboolean isCopyResult;
+    jsize sizeResult = env->GetArrayLength(result);
 
     // 'env' allows access to the JVM, allows to call special JNI functions.
     // 'jobject' or 'jclass' represents the caller of the native method.
-    double* pa = env->GetDoubleArrayElements(aValues, JNI_FALSE); // Pointer to aValues[0]
-    double* pb = env->GetDoubleArrayElements(bValues, JNI_FALSE); // Pointer to bValues[0]
-    double* pr = env->GetDoubleArrayElements(result,  JNI_FALSE);  // Pointer to result[0]
+    double* pa = env->GetDoubleArrayElements(aValues, &isCopyA); // Pointer to aValues[0]
+    double* pb = env->GetDoubleArrayElements(bValues, &isCopyB); // Pointer to bValues[0]
+    double* pr = env->GetDoubleArrayElements(result,  &isCopyResult);  // Pointer to result[0]
 
     multiply(pa, pb, pr, aRows, aCols, bCols);
 
-    env->ReleaseDoubleArrayElements(aValues, pa, 0);
-    env->ReleaseDoubleArrayElements(bValues, pb, 0);
-    env->ReleaseDoubleArrayElements(result, pr, 0);
+    env->SetDoubleArrayRegion(result, 0, sizeResult, pr);
 
-    cout << "C++ segement ended." << endl;
+    if (isCopyA == JNI_TRUE)      { env->ReleaseDoubleArrayElements(aValues, pa, 0); }
+    if (isCopyB == JNI_TRUE)      { env->ReleaseDoubleArrayElements(bValues, pb, 0); }
+    if (isCopyResult == JNI_TRUE) { env->ReleaseDoubleArrayElements(result, pr, 0);  }
+
+    std::cout << "C++ segement ended." << std::endl;
 }
 
 
 JNIEXPORT void JNICALL Java_matrix_1multiplication_Matrix_powerC(JNIEnv* env, jobject o,
-    jdoubleArray vals, jdoubleArray originalMatrix, jint aRows, jint aCols, jint i) {
+    jdoubleArray inp, jdoubleArray resultMatrix, jint aRows, jint i) {
     
-    cout << "Entering powerC function" << endl;
-    cout << "Provided exponent i = " << i << endl;
+    std::cout << "Entering powerC function, provided exponent i = " << i << std::endl;
     
+    jboolean isCopyInp;
+    jboolean isCopyResult;
+
+    jsize result_length = env->GetArrayLength(inp);
+
     // Get input array and result array to write values to.
-    double* pa = env->GetDoubleArrayElements(vals, JNI_FALSE);
-    double* po = env->GetDoubleArrayElements(originalMatrix, JNI_FALSE);
+    jdouble* pa = env->GetDoubleArrayElements(inp, &isCopyInp);
+    jdouble* pr = env->GetDoubleArrayElements(resultMatrix, &isCopyResult);
 
-    // Keeping the result of the k-th multiplication.
-    double* tmp = new double[aRows * aCols];
+    // Result array on heap
+    jdouble* internal = new jdouble[result_length];
+    for (int i = 0; i < result_length; i++) { internal[i] = pa[i]; }
 
-    for (int k = 1; k < i; k++) {
-        multiply(po, pa, tmp, aRows, aCols, aCols);
-        memcpy(tmp, pa, aCols * aRows);
+    for (int k=0; k < i; k++) {
+        multiply(pa, internal, pr, aRows, aRows, aRows);
+                
+        // for (double* pi = pr; pi < pr + result_length; pi++) { std::cout << "k = " << k << ", pi: " << *pi << std::endl; }
+        std::swap(internal, pr);
     }
 
+    env->SetDoubleArrayRegion(resultMatrix, 0, result_length, pr);
 
-    for (int i = 0; i < aRows * aCols; i++) {
-        cout << "tmp[i]: " << tmp[i] << endl;
+    if (isCopyInp    == JNI_TRUE) { env->ReleaseDoubleArrayElements(inp, pa, 0);          }
+    if (isCopyResult == JNI_TRUE) { env->ReleaseDoubleArrayElements(resultMatrix, pr, 0); }
+
+    delete[] internal;
+}
+
+
+
+
+
+
+JNIEXPORT void JNICALL Java_matrix_1multiplication_Matrix_iterateArrayC
+    (JNIEnv* env, jobject o, jdoubleArray inp) {
+
+    double* pi      = env->GetDoubleArrayElements(inp, JNI_FALSE);
+    int lengthInput = env->GetArrayLength(inp);
+    double* end     = pi + lengthInput;
+
+    for (double* i = pi; i != end; i++) {
+        std::cout << "From C++: " << *i << std::endl;
     }
 
-    env->ReleaseDoubleArrayElements(vals, pa, 0);
-    env->ReleaseDoubleArrayElements(originalMatrix, po, 0);
-
-    delete[] tmp;
+    env->ReleaseDoubleArrayElements(inp, pi, 0);
 }
