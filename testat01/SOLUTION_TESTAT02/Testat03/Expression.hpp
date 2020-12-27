668012927e56b884bf6014722cf61269e5723834
mzhKU <ma.hed@bluewin.ch>
2020-12-27 21:03:56 +0100

    TST3_A2

--- testat01/SOLUTION_TESTAT02/Testat03/Expression.hpp
+++ testat01/SOLUTION_TESTAT02/Testat03/Expression.hpp
@@ -42,6 +42,91 @@ public:
 	}
 };
 
+
+// Left is a scalar.
+template<typename Op, typename Right>
+class Expression<typename Right::value_type, Op, Right> {
+	const typename Right::value_type& m_left;
+	const Right& m_right;
+public:
+
+	// The expression value type is given by type of Right.
+	using value_type = typename Right::value_type;
+
+	Expression(const value_type& l, const Right& r) : m_left{ l }, m_right { r }
+	{}
+
+	size_t size() const {
+		return m_right.size();
+	}
+
+	value_type operator[](size_t i) const {
+		return Op::template apply<value_type>(m_left, m_right[i]);
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Expression& e) {
+		os << '[';
+		if (e.size() > 0) { os << e[0]; }
+		for (int i = 1; i < e.size(); i++) {
+			os << ',' << e[i];
+		}
+		return os << ']';
+	}
+
+	template<typename E>
+	bool operator==(const E& e) {
+		for (int i = 0; i < e.size(); i++) {
+			if ((*this)[i] != e[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+
+// Right is a scalar.
+template<typename Left, typename Op>
+class Expression<Left, Op, typename Left::value_type> {
+	const Left& m_left;
+	const typename Left::value_type& m_right;
+public:
+
+	// The expression value type is given by type of Right.
+	using value_type = typename Left::value_type;
+
+	Expression(const Left& l, const value_type& r) : m_left{ l }, m_right{ r }
+	{}
+
+	size_t size() const {
+		return m_left.size();
+	}
+
+	value_type operator[](size_t i) const {
+		return Op::template apply<value_type>(m_left[i], m_right);
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Expression& e) {
+		os << '[';
+		if (e.size() > 0) { os << e[0]; }
+		for (int i = 1; i < e.size(); i++) {
+			os << ',' << e[i];
+		}
+		return os << ']';
+	}
+
+	template<typename E>
+	bool operator==(const E& e) {
+		for (int i = 0; i < e.size(); i++) {
+			if ((*this)[i] != e[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+
 template<typename Left, typename Right>
 Expression<Left, Add, Right> operator+(const Left& l, const Right& r) {
 	return Expression<Left, Add, Right>(l, r);
--- testat01/SOLUTION_TESTAT02/Testat03/Testat03.cpp
+++ testat01/SOLUTION_TESTAT02/Testat03/Testat03.cpp
@@ -7,12 +7,32 @@ using namespace std;
 
 int main()
 {
-	using V4 = Vector<double, 4>;
+
 	cout << "IN TESTAT MAIN." << endl;
+	using V4 = Vector<double, 4>;
+
 	V4 A({ 1, 2, 3, 4 });
 	V4 B({ 2, 1, 0, 1 });
+
+	auto e = (2.0 * (A - B) / 2.0 + B + 5.0) * (A - 4.0 + 4.0 * B) / 4.0;
+	cout << "e = " << e;
+
+	/*
 	Vector<double, 4> D;
-	
+
+	Expression<Vector<int, 3>, Add, Vector<int, 3>> e3 = Vector<int, 3>{ 1, 2, 3 } + Vector<int, 3>{4, 5, 6};
+	cout << "e3 = " << e3 << endl;
+
+	cout << "e3 == e3" << endl;
+	cout << (e3 == e3) << endl;
+
+	Expression<Expression<Vector<int, 3>, Add, Vector<int, 3>>, Add, Vector<int, 3>> e33 = e3 + Vector<int, 3>{4, 5, 6};
+	cout << "e33 = " << e33 << endl;
+
+	using typeOfE3 = typename Expression<Vector<int, 3>, Add, Vector<int, 3>>;
+	Expression<typeOfE3, Add, Vector<int, 3>> e33_2 = e3 + Vector<int, 3>{4, 5, 6};
+	cout << "e33_2 = " << e33_2 << endl;
+
 	auto e = (A - B) * (A + B);
 	D = e;
 	cout << D << endl;
@@ -22,5 +42,5 @@ int main()
 	B = { 4, 3, 3, -2 };
 	cout << e[1] << endl;
 
-
+	*/
 }
